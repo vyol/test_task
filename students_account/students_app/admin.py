f42447ced4182192a5de0bafb2b27ac39310612e
lena <malenka2003@gmail.com>
2010-04-06 09:47:03 +0300

    admin views (students inline)

--- students_account/students_app/admin.py
+++ students_account/students_app/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from students_app.models import Grup, Student
 
-admin.site.register(Grup)
-admin.site.register(Student)
+class StudentInline(admin.TabularInline):
+    model = Student
+    extra = 1
+
+class GrupAdmin(admin.ModelAdmin):
+    inlines = [StudentInline]
+
+admin.site.register(Grup, GrupAdmin)
+
